8d0638d44e805f480caf597299c40540c1fe808a
Diego <draguxen26@gmail.com>
2019-08-03 21:17:44 +0200

    ServicioRestSentimientos - Loading image while resources not loaded

--- ServicioRestSentimientos/src/main/webapp/js/Controllers/api.js
+++ ServicioRestSentimientos/src/main/webapp/js/Controllers/api.js
@@ -1,5 +1,7 @@
 angular.module('restApp').controller('APIController', function($scope, $http, toastr){
 	$scope.titulo = 'Conexión con nuestra API REST';
+	$scope.cargando_algoritmos = true;
+	$scope.cargando_analisis = true;
 	
 	var url = "http://localhost:8080/ServicioRestSentimientos/rest/analisis/api-parameters";
 	
@@ -9,6 +11,8 @@ angular.module('restApp').controller('APIController', function($scope, $http, to
 		$scope.algoritmos = response.data;
 		
 		$scope.txt_area_json_algoritmos = JSON.stringify(response.data, undefined, 4);
+		
+		$scope.cargando_algoritmos = false;
 	});
 	
 	url = "http://localhost:8080/ServicioRestSentimientos/rest/analisis/analize?texto=My house is nice";
@@ -16,5 +20,7 @@ angular.module('restApp').controller('APIController', function($scope, $http, to
 	
 	.then(function(response) {
 		$scope.txt_area_json_analizar = JSON.stringify(response.data, undefined, 4);
+		
+		$scope.cargando_analisis = false;
 	});
 });
